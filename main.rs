8fd2f943e892cf8b435b00565459f4392fc5359a
Damian <d.steer@bris.ac.uk>
2014-04-29 15:27:13 +0100

    Not compiling, but feels close

--- main.rs
+++ main.rs
@@ -3,6 +3,7 @@
 /* Parsing info from http://dds.cr.usgs.gov/srtm/version2_1/SRTM3/ */
 
 use std::io::File;
+use std::vec::Vec;
 
 fn main() {
 
@@ -14,9 +15,22 @@ fn main() {
 	};
 
 	let bytes = match file.read_to_end() {
-		Ok(b) => b,
+		Ok(b) => ~b,
     	Err(e) => fail!("read error: {}", e)
 	};
 
-	println!("Done! {}", bytes);
+	println!("Done! {:?}", bytes);
+
+	let side = ((bytes.len() / 2) as f64).sqrt();
+
+	if side != side.round() {
+		fail!("Not square!");
+	}
+
+	let size = side as uint;
+
+	let heights: Vec<Vec<&u8>> = Vec::from_fn(size, |idy| 
+		Vec::from_fn(size, |idx| bytes.get(idy * size + idx * 2))
+	);
+
 }
