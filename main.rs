1257079e2b348136240764cfecdeb2c11e50f811
Damian <d.steer@bris.ac.uk>
2014-04-30 17:42:12 +0100

    Slow but working

--- main.rs
+++ main.rs
@@ -2,7 +2,7 @@
 
 /* Parsing info from http://dds.cr.usgs.gov/srtm/version2_1/SRTM3/ */
 
-use std::io::File;
+use std::io::{File, IoError};
 use std::vec::Vec;
 
 fn main() {
@@ -29,15 +29,60 @@ fn main() {
 
 	let size = side as uint;
 
-	// Break this out, otherwise we get bytes lifetime issue...
-	let f = |z| bytes.get(z);
-
-	let heights: Vec<Vec<&u8>> = Vec::from_fn(size, |idy| { 
-		Vec::from_fn(size, |idx| f(idy * size + idx * 2))
+	let heights: Vec<Vec<u8>> = Vec::from_fn(size, |idy| { 
+		Vec::from_fn(size, |idx| *bytes.get(idy * size + idx * 2))
 	});
 
 	println!("Done! {:?}", heights);
 
 	println!("Value: {}", heights.get(1).get(1));
 
+	let val = match load(&p) {
+		Ok(v) => v,
+		Err(e) => fail!("Load failed: {}", e)
+	};
+
+	println!("Value: {}", val.get(1).get(1));
+}
+
+fn load(path: &Path) -> Result<Vec<Vec<i16>>, IoError> { 
+	
+	let filesize: u64 = match path.stat() {
+		Ok(stat) => stat.size,
+		Err(e) => fail!("Couldn't read file <{}> size: {}", path.		filename(), e)
+	};
+
+	let sidef = ((filesize / 2) as f64).sqrt();
+
+	if sidef * sidef * 2.0 != (filesize as f64) {
+		fail!("Not square");
+	}
+
+	let side = sidef as uint;
+
+	let mut file = try!(File::open(path));
+
+	let mut result: Vec<Vec<i16>> = Vec::from_fn(side, |idy| { 
+		Vec::with_capacity(side)
+	});
+	
+	let mut counter = 0;
+
+	while !file.eof() {
+		let data = match file.read_be_i16() {
+			Ok(d) => d,
+			Err(e) => return Ok(result)
+		};
+
+		let y = counter / side;
+		let x = counter % side;
+
+		let a = result.get_mut(y);
+
+		a.insert(x,data);
+
+		counter += 1;
+	}
+
+	return Ok(result);
 }
